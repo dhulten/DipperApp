0904497dda074bbe06cc4f4f91d4601386d02fde
dhultengren <david@statdna.com>
2017-04-06 17:36:32 -0400

    Adding notification test

--- app/src/main/java/com/example/dipper/AdminSettingsActivity.java
+++ app/src/main/java/com/example/dipper/AdminSettingsActivity.java
@@ -1,6 +1,12 @@
 package com.example.dipper;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.TaskStackBuilder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -40,5 +46,35 @@ public class AdminSettingsActivity extends AppCompatActivity {
         } catch (Exception ex) {
             ((TextView) findViewById(R.id.saveResults)).append("Error saving settings");
         }
+    }
+
+    public void testPush(View view){
+        NotificationCompat.Builder mBuilder =
+                new NotificationCompat.Builder(this)
+                        .setSmallIcon(R.drawable.notification)
+                        .setContentTitle("My notification")
+                        .setContentText("Hello World!");
+
+        // Creates an explicit intent for an Activity in your app
+        Intent resultIntent = new Intent(this, MainActivity.class);
+
+        // The stack builder object will contain an artificial back stack for the
+        // started Activity.
+        // This ensures that navigating backward from the Activity leads out of
+        // your application to the Home screen.
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+        // Adds the Intent that starts the Activity to the top of the stack
+        stackBuilder.addNextIntent(resultIntent);
+        PendingIntent resultPendingIntent =
+                stackBuilder.getPendingIntent(
+                        0,
+                        PendingIntent.FLAG_UPDATE_CURRENT
+                );
+                mBuilder.setContentIntent(resultPendingIntent);
+                NotificationManager mNotificationManager =
+                        (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        int notifyId = 1;
+        mNotificationManager.notify(notifyId, mBuilder.build());
+
     }
 }
