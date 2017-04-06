4befc729b230bc3df52e95e1aaff2cc8dd791ed3
dhultengren <david@statdna.com>
2017-04-06 11:45:12 -0400

    adding user settings save functionality

--- app/src/main/java/com/example/dipper/Constants.java
+++ app/src/main/java/com/example/dipper/Constants.java
@@ -16,4 +16,10 @@ public class Constants {
     public static final String LocalImageName = "/image.jpg";
     public static final String Data = "Data";
     public static final String DateStr = "DateStr";
+    public static final int CheckinIntervalHrs = 30;// check in should be daily but allow for variation in time
+    public static final String Preferences = "Preferences";
+    public static final String UserPushNotificationsKey = "UserPushNotificationsKey";
+    public static final String AdminPushNotificationsKey = "AdminPushNotificationsKey";
+    public static final String AdminModeKey = "AdminModeKey";
+    public static final String UserAlertTimeKey = "UserAlertTimeKey";
 }
--- app/src/main/java/com/example/dipper/SettingsActivity.java
+++ app/src/main/java/com/example/dipper/SettingsActivity.java
@@ -1,5 +1,6 @@
 package com.example.dipper;
 
+import android.content.SharedPreferences;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
@@ -11,6 +12,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Switch;
 import android.widget.TextView;
 
 public class SettingsActivity extends AppCompatActivity {
@@ -19,5 +21,50 @@ public class SettingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        SharedPreferences settings = getApplicationContext().getSharedPreferences(Constants.Preferences, 0);
+
+        ((Switch)findViewById(R.id.pushNotificationsSwitch)).setChecked(settings.getBoolean(Constants.UserPushNotificationsKey, false));
+        ((TextView)findViewById(R.id.alertTimeVal)).append(String.valueOf(settings.getInt(Constants.UserAlertTimeKey, 60)));
+    }
+
+    public void saveSettings(View view){
+        boolean pushNotificaitonsSetting = ((Switch)findViewById(R.id.pushNotificationsSwitch)).isChecked();
+        boolean alertTimeParseSuccess = tryParse(((TextView)findViewById(R.id.alertTimeVal)).getText().toString());
+
+        findViewById(R.id.saveResults).setVisibility(View.VISIBLE);
+        ((TextView) findViewById(R.id.saveResults)).setText("");
+
+        if (alertTimeParseSuccess) {
+            try {
+                int alertTimeVal = Integer.parseInt(((TextView) findViewById(R.id.alertTimeVal)).getText().toString());
+
+                SharedPreferences settings = getApplicationContext().getSharedPreferences(Constants.Preferences, 0);
+                SharedPreferences.Editor editor = settings.edit();
+
+                editor.putBoolean(Constants.UserPushNotificationsKey, pushNotificaitonsSetting);
+                editor.putInt(Constants.UserAlertTimeKey, alertTimeVal);
+                editor.apply();
+
+                ((TextView) findViewById(R.id.saveResults)).append("Settings saved successfully!");
+            } catch (Exception ex) {
+                ((TextView) findViewById(R.id.saveResults)).append("Error saving settings");
+            }
+
+
+        }
+        else {
+            ((TextView)findViewById(R.id.saveResults)).append("Alert time must be formatted as a number!");
+        }
+
+    }
+
+    private boolean tryParse(String s) {
+        try {
+            Integer.parseInt(s);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
     }
 }
